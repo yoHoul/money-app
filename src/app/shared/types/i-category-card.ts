9c333dfdee9a0feb831346dcc309f498c335a4c8
yoHoul <mrleorideik@gmail.com>
2025-01-30 00:14:31 +0600

    categories devs

--- src/app/shared/types/i-category-card.ts
+++ src/app/shared/types/i-category-card.ts
@@ -7,23 +7,14 @@ export interface ICard {
     CardName: string;
     CardLogo: string;
     CardColor: string;
-    CardWorth: number;
+    CardStory: ICardStory[] | null;
 }
 
-export interface logs {
+export interface ICardStory {
     date: string;
     value: number;
 }
 
-export enum CardLogo {
-    ForkAndKnife = 'ForkAndKnife',
-    Ticket = 'Ticket'
-}
-
-export enum CardColor {
-    Pink = 'Pink',
-    Blue = 'Blue',
-    Green = 'Green',
-    Red = 'Red',
-    Black = 'Black'
+export interface ICategories extends ICard {
+    networth: number;
 }
